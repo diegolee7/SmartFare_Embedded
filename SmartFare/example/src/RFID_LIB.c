0e6bb989383b3b9f3137bdb11e45340ce86e1f00
luisfg30-PC\luisfg30 <luisfg30@hotmail.com>
2016-09-22 10:32:35 -0300

    #2: Completed conversion from C++ to C

--- SmartFare/example/src/RFID_LIB.c
+++ SmartFare/example/src/RFID_LIB.c
@@ -15,6 +15,7 @@
 
 // TODO: insert other include files here
 #include "delay.h"
+#include "MFRC522.h"
 // TODO: insert other definitions and declarations here
 
 int main(void) {
@@ -27,7 +28,17 @@ int main(void) {
     Board_Init();
 
     // TODO: insert code here
-    SysTick_Init();//to use the daley libray
+    SysTick_Init();//to use the delay library
+    MFRC522Ptr_t mfrc1;
+    MFRC522_Init(mfrc1);
+    //Define the pins to use as CS(SS or SSEL)  an RST
+    // GPIO1[8]= P1_5
+    mfrc1->_chipSelectPin.port = 1;
+    mfrc1->_chipSelectPin.pin = 8;
+    // GPIO3[4]= P6_5
+    mfrc1->_resetPowerDownPin.port = 3;
+    mfrc1->_resetPowerDownPin.pin = 4;
+    PCD_Init(mfrc1);
 
     while(1) {
 		delay_ms(2000);
